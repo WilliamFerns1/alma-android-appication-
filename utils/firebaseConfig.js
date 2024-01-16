cd3c33ae7b5662b00e1e5ec23c16644fa5c1a5bd
William Ferns <willferns2000@gmail.com>
2024-01-16 14:37:49 +0200

    function made to create document on login, if it doesn't already exist

---
+++ utils/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAxZ9cG0FkkLggIvtZg_CMzCDRnYfOVjr4",
+    authDomain: "alma-3796a.firebaseapp.com",
+    projectId: "alma-3796a",
+    storageBucket: "alma-3796a.appspot.com",
+    messagingSenderId: "311932612201",
+    appId: "1:311932612201:web:e5f3bb451ded866f79fcf8"
+};
+
+
